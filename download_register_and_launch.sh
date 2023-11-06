4ec392494d02d5ea60b6ebdfd7b2bef75bb5e08b
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2023-11-06 20:43:55 +0100

    setting up docker-based huggingface model deplyement using xinference

---
+++ download_register_and_launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 5
+python login_and_download.py
+xinference register --persist --file /config.json
+xinference launch --model-name $LLM_NAME --size-in-billions $LLM_SIZE --model-format $LLM_FORMAT --n-gpu $N_GPUS
